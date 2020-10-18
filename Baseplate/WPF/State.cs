44b33dcfa4ad260794a4307dde7073a505b8eb9a
amitlzkpa <amit.lzkpa@gmail.com>
2020-10-17 20:46:44 -0500

    add state singleton

---
+++ Baseplate/WPF/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    class State
+    {
+        private State() { }
+
+        private static State _instance = null;
+
+        public static State instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new State();
+                }
+                return _instance;
+            }
+        }
+
+    }
+}
